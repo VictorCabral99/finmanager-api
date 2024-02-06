9c726657b7872cdfe705830b24e23841b849e728
Victor Cabral <vhcabral99@gmail.com>
2024-02-05 23:52:28 -0300

    first commit

---
+++ src/main/java/br/com/finmanager/FinmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.finmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinmanagerApplication.class, args);
+	}
+
+}
